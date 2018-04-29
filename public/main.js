f9ab293687baad953d08c7d0d80c88abf85a1c95
Jon Collins <joncollins@nh-lon-c02vm0fxhtd6.home>
2018-04-29 14:33:21 +0100

    DB update creates array

--- public/main.js
+++ public/main.js
@@ -12,7 +12,6 @@ window.onload = function() {
   firebase.initializeApp(config);
 
   const database = firebase.database().ref();
-  // let databaseRef = database.ref()
 
   // Set HTML Variables
   const questionForm = document.getElementById('post-form');
@@ -20,69 +19,81 @@ window.onload = function() {
   const submitButton = document.getElementById('submit-button');
   const readOut = document.getElementById('read-out');
   // Variable to store the synced DB data
-  let dbObj = {};
+  var dbObj = {};
+  var words = [];
 
-  //Gets the database data when it changes and stores it
-  database.on('value', function(snap){
-    dbObj = snap;
-    readOut.innerText = JSON.stringify(snap);
-    console.log(JSON.stringify(snap));
-  });
+  //Variable that sets where writeUserData stores the data in the DB
+  var wordRef = firebase.database().ref('words');
 
-  //Function to post a new word into the DB
-
-  // function writeUserData(formWord, wordCount) {
-  //   entry = `{${formWord}: ${wordCount}}`;
-  //   database.set(entry);
-  // }
-
-//Concern that it will come out of sync -> Check thinking.
-// function writeUserData(formWord) {
-//   let newWordCount = null;
-//   console.log(dbObj)
-//   if (dbObj.hasOwnProperty(formWord)) {
-//     newWordCount = dbObj[formWord].value  + 1;
-//   } else {
-//     newWordCount = 1;
-//   }
-//   firebase.database().ref(formWord).set(newWordCount);
-// }
-
-var wordRef = firebase.database().ref('words');
-function writeUserData(input) {
-  console.log('in writeUserData', arguments);
-  wordRef.transaction(function(currentData) {
-    console.log('currentData', currentData);
-    console.log('currentData[input]', currentData[input]);
-    const val = currentData[input];
-    if (!val && typeof val !== 'number') { //might need to hasOwnProperty this
-      return {
-        ...currentData,  //PROBABLY NEED TO FIX THIS AS IT INTRODUCES A RACE CONDITION
-        [input]: 1
-      };
-    } else {
-      console.log(input + ' already exists.');
-      return {
-        ...currentData,
-        [input]: val + 1
-      };
-    }
-  }, function(error, committed, snapshot) {
-  if (error) {
-    console.log('Transaction failed abnormally!', error);
-  } else if (!committed) {
-    console.log('We aborted the transaction (because ada already exists).');
-  } else {
-    console.log('User ada added!');
+  //Function to post a new word into the DB or increment existing
+  function writeUserData(input) {
+    console.log('in writeUserData', arguments);
+    wordRef.transaction(function(currentData) {
+      console.log('currentData', currentData);
+      console.log('currentData[input]', currentData[input]);
+      const val = currentData[input];
+      if (!val && typeof val !== 'number') {
+        return {
+          ...currentData,  //PROBABLY NEED TO FIX THIS AS IT INTRODUCES A RACE CONDITION
+          [input]: 1
+        };
+      } else {
+        console.log(input + ' already exists.');
+        return {
+          ...currentData,
+          [input]: val + 1
+        };
+      }
+    }, function(error, committed, snapshot) {
+      if (error) {
+        console.log('Transaction failed abnormally!', error);
+      } else if (!committed) {
+        console.log('We aborted the transaction (because ada already exists).');
+      } else {
+        console.log('Word added..');
+      }
+      console.log("The data: ", snapshot.val());
+    });
   }
-  console.log("Ada's data: ", snapshot.val());
-});
-}
 
-questionForm.addEventListener('submit', function(e) {
+  //Passes the question field data into the database via writeUserData
+  questionForm.addEventListener('submit', function(e) {
     // writeUserData(answerForm.value);
     e.preventDefault();
-    let theWord = answerTextbox.value;
-    writeUserData(theWord);
-});
-}
+    let userInput = answerTextbox.value;
+    writeUserData(userInput);
+  });
+
+  //Gets the database data when it changes and stores it as an array
+  database.on('value', function(snap){
+    dbObj = snap.child('words').val();
+    var tempArray = [];
+    for (var key in dbObj) {
+      if (dbObj.hasOwnProperty(key)) {
+        console.log('for in key result ' + dbObj[key] + ' key: ' + key);
+        tempArray.push(`{text: '${key}', size: ${dbObj[key]}}`);
+      }
+    }
+    console.log('tempArray: '+ tempArray);
+    words = tempArray.map(i => i);
+    readOut.innerText = JSON.stringify(dbObj);
+    console.log('words: ' + words);
+    d3.wordcloud()
+      .size([800, 400])
+      .selector('#wordcloud')
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      // .words([{text: 'word', size: 5}, {text: 'cloud', size: 15}])
+      .words(words)
+      .start();
+  })
+
+
+  // d3.wordcloud()
+  //   .size([800, 400])
+  //   .selector('#wordcloud')
+  //   .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+  //   // .words([{text: 'word', size: 5}, {text: 'cloud', size: 15}])
+  //   .words(words)
+  //   .start();
+
+};
